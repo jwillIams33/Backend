316f7b2c2bfc6973d1718e97ba0d09105b011499
Williams <jamal.williams@wwt.com>
2020-10-11 04:31:17 -0500

    add backend

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,8 +1,12 @@
-import { v4 } from 'https://deno.land/std/uuid/mod.ts';
-import { Product } from '../types.ts';
-import { RouterContext } from 'https:deno.land/x/oak@v6.3.1/mod.ts';
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
+import { Product } from "../types.ts";
+import {
+  RouterContext,
+  HttpError,
+  Status,
+} from "https:deno.land/x/oak@v6.3.1/mod.ts";
 
-import { CourseGoal } from '../models/product.ts';
+import { CourseGoal } from "../models/product.ts";
 
 type RContext = RouterContext<
   Record<string | number, string | undefined>,
@@ -11,38 +15,77 @@ type RContext = RouterContext<
 
 let products: Product[] = [];
 
-const getProducts = (ctx: RContext) => {
-  ctx.response.body = {
-    success: true,
-    data: products,
-  };
+// const getProducts = (ctx: RContext) => {
+//   ctx.response.body = {
+//     success: true,
+//     data: products,
+//   };
+// };
+
+const getProducts = async (ctx: RContext) => {
+  try {
+    products = await CourseGoal.findAll();
+    console.log(products);
+    ctx.response.body = {
+      products,
+    };
+  } catch (err) {
+    const error = new HttpError();
+    error.status = Status.NotFound;
+    throw error;
+  }
+};
+
+const getSingleProduct = async (ctx: RContext) => {
+  try {
+    const id = ctx.params.id!;
+    console.log(id);
+    const goal = await CourseGoal.findById(id);
+    if (!goal) {
+      const error = new HttpError();
+      error.status = Status.NotFound;
+      throw error;
+    }
+
+    ctx.response.body = {
+      goal,
+    };
+  } catch (err) {
+    const error = new HttpError();
+    error.status = Status.NotFound;
+    throw error;
+  }
 };
 
 // Get A Product @route GET /API/products
-const getProduct = (
-  { params, response }: { params: { id: string }; response: any },
-) => {
-  const product: Product | undefined = products.find((p) => p.id === params.id);
+// const getProduct = ({
+//   params,
+//   response,
+// }: {
+//   params: { id: string };
+//   response: any;
+// }) => {
+//   const product: Product | undefined = products.find((p) => p.id === params.id);
 
-  if (product) {
-    response.status = 200;
-    response.body = {
-      success: true,
-      data: product,
-    };
-  } else {
-    response.status = 404;
-    response.body = {
-      success: false,
-      msg: "No Product Found",
-    };
-  }
-};
+//   if (product) {
+//     response.status = 200;
+//     response.body = {
+//       success: true,
+//       data: product,
+//     };
+//   } else {
+//     response.status = 404;
+//     response.body = {
+//       success: false,
+//       msg: "No Product Found",
+//     };
+//   }
+// };
 
 // Add A Product @route POST /API/products
 const addProduct = async (ctx: RContext) => {
   const body = await ctx.request.body();
-  if(body.type === "json"){
+  if (body.type === "json") {
     const value = await body.value;
     console.log(value);
   }
@@ -70,13 +113,15 @@ const addProduct = async (ctx: RContext) => {
 };
 
 // Update A Product @route PUT /API/products/:id
-const updateProduct = async (
-  { params, request, response }: {
-    params: { id: string };
-    request: any;
-    response: any;
-  },
-) => {
+const updateProduct = async ({
+  params,
+  request,
+  response,
+}: {
+  params: { id: string };
+  request: any;
+  response: any;
+}) => {
   const product: Product | undefined = products.find((p) => p.id === params.id);
 
   if (product) {
@@ -104,14 +149,27 @@ const updateProduct = async (
 };
 
 // Delete A Product @route DELETE /API/products/:id
-const deleteProduct = (
-  { params, response }: { params: { id: string }; response: any },
-) => {
+const deleteProduct = async ({
+  params,
+  response,
+}: {
+  params: { id: string };
+  response: any;
+}) => {
+  const id = params.id!;
   products = products.filter((p) => p.id !== params.id);
+  console.log(id);
+  try {
+    await CourseGoal.delete(id);
+  } catch (err) {
+    const error = new HttpError();
+    error.status = Status.NotFound;
+    throw error;
+  }
   response.body = {
     success: true,
     msg: "Product Removed",
   };
 };
 
-export { getProducts, getProduct, addProduct, updateProduct, deleteProduct };
+export { getProducts, getSingleProduct, addProduct, updateProduct, deleteProduct };
--- models/product.ts
+++ models/product.ts
@@ -13,5 +13,43 @@ export class CourseGoal {
         throw new Error('Failed to store goal!');
       }
     }
+
+    static async findAll() {
+        try {
+          const goalDocs = await getGoalsCollection()!.find();
+          return goalDocs;
+        } catch (err) {
+          console.log(err);
+          throw new Error('Failed to find goal!');
+        }
+      }
+
+      static async findById(
+        id: string
+      ): Promise<{ name: string; _id: { $oid: string } } | undefined> {
+        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+          throw new Error('Could not find goal by id.');
+        }
+        try {
+          const goalDoc = await getGoalsCollection()!.findOne({
+            _id: { $oid: id },
+          });
+          return goalDoc;
+        } catch (err) {
+          throw new Error('Could not find goal by id.');
+        }
+      }
+
+    static async delete(id: string) {
+        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+          throw new Error('Could not find goal by id.');
+        }
+        try {
+          await getGoalsCollection()!.deleteOne({ _id: { $oid: id } });
+        } catch (err) {
+          console.log(err);
+          throw new Error('Failed to delete goal.');
+        }
+      }
 }
   
